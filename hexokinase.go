96e65ef1fc8080fd274186bea505e5abfcccdcaf
Adam P. Regasz-Rethy <rethy.spud@gmail.com>
2019-04-15 21:05:33 -0400

    Read text either Stdin or command-line file args

--- hexokinase.go
+++ hexokinase.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("vim-go")
+	files := os.Args[1:]
+	if len(files) != 0 {
+		for _, fname := range files {
+			f, err := os.Open(fname)
+			if err != nil {
+				continue
+			}
+			parse(f)
+			f.Close()
+		}
+	} else {
+		parse(os.Stdin)
+	}
+}
+
+func parse(f *os.File) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		text := input.Text()
+		fmt.Println(text)
+	}
 }
